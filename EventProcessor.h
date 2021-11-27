e72956affc3de2042c1b0adcc96f24af727cef37
Taha321 <93052614+Taha321@users.noreply.github.com>
2021-11-26 23:33:20 -0500

    Update EventProcessor.h

--- EventProcessor.h
+++ EventProcessor.h
@@ -1,12 +1,22 @@
 #ifndef _EVENTPROCESSOR_H
 #define _EVENTPROCESSOR_H
 
-
-enum keycode
+enum keycode : uint8_t
 {
-    key_A = 0, key_B = 1, key_X = 2, key_Y = 3,
-    Right = 4, Left = 5, Up = 6, Down = 7,
-    LT = 8, RT = 9
+    key_A = 0, 
+    key_B = 1, 
+    key_X = 2, 
+    key_Y = 3,
+    Right = 4, 
+    Left = 5, 
+    Up = 6, 
+    Down = 7,
+    LT = 8, 
+    RT = 9, 
+    RightJoyStick_X = 10, 
+    RightJoyStick_Y = 11,
+    LeftJoyStick_X = 12, 
+    LeftJoyStick_Y = 13
 };
 struct Event
 {
@@ -16,7 +26,16 @@ struct Event
         Release
     };
     type eventType;
-    unsigned int code;
+    uint8_t code;
+};
+
+struct JoyStick
+{
+    int ThreadID;
+    uint8_t codeX;
+    uint8_t codeY;
+    uint16_t x;
+    uint16_t y;
 };
 
 
