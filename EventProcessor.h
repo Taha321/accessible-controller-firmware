71d8999bbb2f485885611c5536c838f855c95663
rmione <rmione33@gmail.com>
2022-01-07 18:23:37 -0500

    Add initial changes from collaborative code session

--- EventProcessor.h
+++ EventProcessor.h
@@ -1,9 +1,11 @@
 #ifndef _EVENTPROCESSOR_H
 #define _EVENTPROCESSOR_H
+#include "XInput.h"
+
 
 enum keycode : uint8_t
 {
-    key_A = 0, 
+    key_A = 0,
     key_B = 1, 
     key_X = 2, 
     key_Y = 3,
@@ -38,5 +40,48 @@ struct JoyStick
     uint16_t y;
 };
 
+class EventProcessor 
+{
+    /* 
+    Take events off the queue and process them, transforming them into
+    Xinput presses.
+    */
+   public: 
+    void begin(EventQueue<Event>& queue) 
+    {
+        EQ = queue;
+    }
+    
+    // Event getEvent()
+    // {
+    //   Event e = {0};
+    //   EQ->getQ(e)
+    //   return e;
+    // }    
+    void ProcessEvent_XInput()
+    {
+      Event e;
+      while(true)
+      {
+        if(EQ->getQ(e)) // Will return true if there is an event on the queue, false if it's empty
+        {
+          uint8_t code = mapping[e.code]; // Fetch mapping from private variable
+          if e.type == Press 
+            Xinput.press(code);
 
+          else if e.type == Release 
+            Xinput.release(code);
+          
+        }
+      }
+    }
+
+    
+   private: 
+     static EventQueue<Event>* EQ;
+     uint8_t mapping[] = {BUTTON_A, BUTTON_B, BUTTON_X, BUTTON_Y, DPAD_RIGHT, DPAD_LEFT, DPAD_UP, DPAD_DOWN, TRIGGER_LEFT, TRIGGER_RIGHT, JOY_LEFT, JOY_LEFT, JOY_RIGHT, JOY_RIGHT}
+
+     
+
+};
 #endif
--- README.md
+++ README.md
@@ -4,3 +4,7 @@
     - https://github.com/abrox/eventqueue
     - https://github.com/wizard97/SimplyAtomic
 3. Compile/verify and upload to Teensy 3.2
+
+# Contribution Instructions 
+- When you are making a branch, it should be of the format ```username/branch-name```
+- Make a pull request, when all bugs are ironed out,  and code review has been done, merge branch back to ```main```.
